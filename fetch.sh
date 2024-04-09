6558c7771858a66718314821bc24e70a1854026e
Jonathan <jonathan@laster.dev>
2024-04-09 09:07:42 -0400

    Update

--- fetch.sh
+++ fetch.sh
@@ -1,79 +1,68 @@
 #!/bin/bash
-# Navigates through each github repo in my directory and runs `git pull --rebase`
 
-main_repo_dir="/home/$USER/code"
+set -e
+
+red="\033[31m"
+reset="\033[0m"
+green="\033[32m"
+yellow="\033[93m"
+magenta="\033[35m"
+
 dir_absolute_paths=()
+main_repo_dir="/home/${USER}/code"
 
-# If on Mac, change `main_repo_dir` to `/Users/$USER/code`
 if [ $(uname) == "Darwin" ]; then
-  main_repo_dir="/Users/$USER/code"
+	main_repo_dir="/Users/${USER}/code"
 fi 
 
-# Runs git commands on each branch of the repo
-function git_cmds {
-    # Add all branches in the repo to an array
+git_cmds() {
     branches=()
     branches+=$(git branch -l | awk '$1 == "*" {print $2} $1 != "*" {print $1}')
 
-    # For each branch, switch to it first and then run command
-    for branch in $branches; do
-      git switch $branch
-      git pull --rebase 2>/dev/null
+    for branch in ${branches}; do
+		git switch ${branch}
+		git pull --rebase 2>/dev/null
 
-      # Cleaning up stderr that is displayed. Less clutter, directly to the point.
-      exit_status=$?
-      if [ $exit_status -eq 128 ]; then
-        echo -e "\n\t\033[31mCannot pull with rebase: You have unstaged changes.\033[0m\n"
-      fi
+		exit_status=$?
+		if [ ${exit_status} -eq 128 ]; then
+        	echo "\n\t${red}Cannot pull with rebase: You have unstaged changes.${reset}\n"
+		fi	
     done
 }
 
-# Clear screen first
-clear -x
-
-# Pretty ASCII art :) 
-echo -e '\033[32m
+echo "${green}
 ================================================================
 ________                          __________    _____      ______  
 ___  __ \__________________       ___  ____/______  /_________  /_ 
-__  /_/ /  _ \__  __ \  __ \________  /_   _  _ \  __/  ___/_  __ \
+__  /_/ /  _ \__  __ \  __ \________  /_   _  _ \  __/  ___/_  __ \\
 _  _, _//  __/_  /_/ / /_/ //_____/  __/   /  __/ /_ / /__ _  / / /
 /_/ |_| \___/_  .___/\____/       /_/      \___/\__/ \___/ /_/ /_/ 
              /_/                                                                        
 ================================================================
-\033[0m'
+${reset}"
 
-echo -e "> \033[95mMoving to repo directory:\033[93m $main_repo_dir\033[0m"
-cd "$main_repo_dir"
-sleep 0.5
-echo
-echo -e "> \033[95mGathering child directories...\033[0m"
-echo
-sleep 0.5
+echo "> ${magenta}Moving to repo directory:${yellow} ${main_repo_dir}${reset}"
+cd "${main_repo_dir}"
 
-# Iterate through each directory that is a git repo and return an array of their absolute paths
+echo "\n> ${magenta}Gathering child directories...${reset}\n"
 for dir in $(ls); do
-  cd $main_repo_dir/$dir
-  if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-    dir_absolute_paths+=("$(pwd)")
-  fi
+	cd ${main_repo_dir}/${dir}
+	if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+		dir_absolute_paths+=("$(pwd)")
+	fi
 done
 
-# Move into dedicated project directory and add it's git repos to the array
-cd "$main_repo_dir/projects"
+cd "${main_repo_dir}/projects"
 for dir in $(ls); do
-  cd $main_repo_dir/projects/$dir
-  if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
-    dir_absolute_paths+=("$(pwd)")
-  fi
+	cd ${main_repo_dir}/projects/${dir}
+	if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+		dir_absolute_paths+=("$(pwd)")
+	fi
 done
 
-# Navigate to each path in the array and run commands
 for path in ${dir_absolute_paths[@]}; do
-  echo -e "> \033[95mMoving to: \033[93m$path\033[0m"
-  cd $path
-  git_cmds
-  echo
+	echo "> ${magenta}Moving to: ${yellow}${path}${reset}"
+	cd ${path}
+	git_cmds
+	echo
 done
-
-exit 0
